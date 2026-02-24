d7c4149f88884115b6fedd6fe1e8ead2d853c2bd
kgabba <yuzhak1997@gmail.com>
2026-02-24 13:14:44 +0000

    fixed dock-comp & routers

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,5 +1,4 @@
-from fastapi.routing import APIRoute
-from main import api
+from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from models.model import UsersTable, TeamsTable
 from fastapi import HTTPException
@@ -7,13 +6,15 @@ from models.model import Base
 from models.schemas import User, Team
 
 
-router_basic = APIRoute(prefix='/do')
+router_basic = APIRouter(prefix='/do')
 
 mapped_dict = {'users' : UsersTable,
                'teams' : TeamsTable}
 
 @router_basic.get('/{table_name}')
 def get_data_from_table(table_name: str):
+    from main import api
+
     class_table = mapped_dict.get(table_name)
     if not class_table:
         raise HTTPException(status_code=404, detail=f'table {table_name} not found in db')
@@ -27,6 +28,8 @@ def get_tables_name():
 
 @router_basic.post('/add_user')
 def add_user_db(user: User):
+    from main import api
+
     new_user = UsersTable(username=user.username,
                           team_id=user.team_id,
                           rating=user.rating,
@@ -38,6 +41,8 @@ def add_user_db(user: User):
 
 @router_basic.post('/add_team')
 def add_team_db(team: Team):
+    from main import api
+
     new_team = TeamsTable(name=team.name)
     with Session(api.state.eng) as session:
         session.add(new_team)
